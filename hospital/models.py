5c1be3b3179c67d5b5bb31e0b5726e139c7b0075
Sardor <sardor.tozhiev1994@gmail.com>
2026-07-16 13:02:34 +0300

    add CustomUser, Department and Symptom models

--- hospital/models.py
+++ hospital/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('patient', 'Patient'),
+        ('doctor', 'Doctor'),
+    ]
+    role = models.CharField(
+        max_length=50,
+        choices=ROLE_CHOICES,
+        default='patient',
+    )
+
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    floor = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} (Floor: {self.floor})"
+
+
+class Symptom(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+
